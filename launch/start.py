45f13c9088f215dd9fda706f6a29d2d5ce30f0d8
WilliamDubois <william.dubois@usherbrooke.ca>
2025-06-25 10:40:17 +0200

    working config for RS-Airy lidar

--- launch/start.py
+++ launch/start.py
@@ -6,7 +6,7 @@ def generate_launch_description():
 
     rviz_config=get_package_share_directory('rslidar_sdk')+'/rviz/rviz2.rviz'
 
-    config_file = '' # your config file path
+    config_file = get_package_share_directory('rslidar_sdk')+'/config/config.yaml' # your config file path
 
     return LaunchDescription([
         Node(namespace='rslidar_sdk', package='rslidar_sdk', executable='rslidar_sdk_node', output='screen', parameters=[{'config_path': config_file}]),
